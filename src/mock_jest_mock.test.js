a936daec802fc1ed285194c8ffa2bb2a597976e9
arimohy <arimohy@gmail.com>
2021-10-05 22:57:23 -0500

    implementacion de los test

---
+++ src/mock_jest_mock.test.js
@@ -0,0 +1,12 @@
+import * as math from "./math";
+import * as app from "./App"
+// Set all module functions to jest.fn
+jest.mock("./math.js");
+test("calls math.add", () => {
+    app.doAdd(1, 2);
+    expect(math.add).toHaveBeenCalledWith(1, 2);
+});
+test("calls math.subtract", () => {
+    app.doSubtract(1, 2);
+    expect(math.subtract).toHaveBeenCalledWith(1, 2);
+});
